5765b3bdd7702d6cc70bc2a075cf87832dcb1f98
Nathan Gang <nathan.gang@limitbreak.com>
2024-08-16 16:56:44 -0600

    Add invariant test suite for Wrapped Native that covers all the functions available.

--- src/IWrappedNative.sol
+++ src/IWrappedNative.sol
@@ -7,10 +7,10 @@ interface IWrappedNative {
 
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external payable returns (bool);
     function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external payable returns (bool);
+    function transferFrom(address from, address to, uint256 value) external payable returns (bool);
 
     // ERC20 Metadata Specific
     function name() external view returns (string memory);
