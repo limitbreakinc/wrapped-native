95fecfc25e885f3d26f7ac174522cae01ba6847f
Nathan Gang <nathan.gang@limitbreak.com>
2024-08-19 15:13:25 -0600

    Change to 0.8.25 targeting cancun

--- src/IRecoverTokens.sol
+++ src/IRecoverTokens.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.26;
+pragma solidity 0.8.25;
 
 interface IRecoverTokens {
     function transfer(address /*_to*/, uint256 /*_value*/) external returns (bool);
--- test/mocks/ContractMockRejectsNative.sol
+++ test/mocks/ContractMockRejectsNative.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.26;
+pragma solidity 0.8.25;
 
 contract ContractMockRejectsNative {
     constructor() {}
